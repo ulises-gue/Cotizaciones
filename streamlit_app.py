99472aef85d930b5121aaba178ce37f4d63114d9
ulises-gue <111939337+ulises-gue@users.noreply.github.com>
2025-03-11 23:33:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
 st.title("Cotizaciones - Border Freight")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
-input("Enter your Name: ")
+
+st.number_input("Ingerese el kilometraje de la Ruta: ")
