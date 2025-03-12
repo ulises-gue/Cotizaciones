39f650b20c2275d8c3baca9ccd4a9dedd9ca25c1
ulises-gue <111939337+ulises-gue@users.noreply.github.com>
2025-03-11 23:41:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ costo_por_km = 25.3
 #La primer variable que necesitamos sabe es el kilometraje de la ruta
 st.number_input("Ingerese el kilometraje de la Ruta: ", min_value = 1, step = 1)
 #Despues se pregunta si qusiera cotizar en dolares
-st.radio("Cotizar en USD?", ("No","Si"))
+dolares = st.radio("Cotizar en USD?", ("No","Si"))
 
 if dolares == "No":
     sugerir_precio = st.radio("¿Tienes algún precio en mente?", ("No", "Sí"))
