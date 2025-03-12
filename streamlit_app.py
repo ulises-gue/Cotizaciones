bf40e901c0688e0033d17725cafb9cd6879c55c0
ulises-gue <111939337+ulises-gue@users.noreply.github.com>
2025-03-11 23:29:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Cotizaciones - Border Freight")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+input("Enter your Name: ")
