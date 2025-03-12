e32d73041770e50fc5227e9c2d8a651f0a364f74
ulises-gue <111939337+ulises-gue@users.noreply.github.com>
2025-03-11 23:37:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,14 @@ import streamlit as st
 
 st.title("Cotizaciones - Border Freight")
 
-st.number_input("Ingerese el kilometraje de la Ruta: ")
+#Este es el costo por kilometro que usaremos para el caluculo de rentabilidad de ruta
+costo_por_km = 25.3
+
+#La primer variable que necesitamos sabe es el kilometraje de la ruta
+st.number_input("Ingerese el kilometraje de la Ruta: ", min_value = 1, step = 1)
+#Despues se pregunta si qusiera cotizar en dolares
+st.radio("Cotizar en USD?", ("No,"Si"))
+
+                             
+
+
